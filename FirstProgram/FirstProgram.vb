9cd8eb4ac3d3998de5d665531803d3e270fbb706
Jamison Burton <jamison@burtontalk.com>
2022-01-11 08:40:48 -0700

    Add header

--- FirstProgram/FirstProgram.vb
+++ FirstProgram/FirstProgram.vb
@@ -1,4 +1,10 @@
-﻿Module FirstProgram
+﻿'Jamison Burton
+'Rcet0265
+'Spring 2022
+'FirstProgram.vb
+'https://github.com/Reimonoe17/FirstProgram/blob/master/FirstProgram/FirstProgram.vb
+
+Module FirstProgram
 
     Sub Main()
         Console.WriteLine("This is DECLO!")
